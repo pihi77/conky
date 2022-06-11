a51e1289f6bc84763f44a035d0fa83d46dd80037
Marek Mihok <marek.mihok@radixal.net>
2022-06-11 21:35:33 +0200

    pridane fonty

--- conky_start.sh
+++ conky_start.sh
@@ -1,5 +1,10 @@
 cd $(dirname $0)
 killall conky
+mkdir ~/.fonts > /dev/null 2>&1 
+cp fonts/*.*tf ~/.fonts > /dev/null 2>&1 
+mkdir ~/.local/share/fonts/ > /dev/null 2>&1 
+cp fonts/*.*tf ~/.local/share/fonts/ > /dev/null 2>&1 
+fc-cache ~/.fonts                                     
 conky -q -c cpu.conf |
 conky -q -c infofile.conf | 
 conky -q -c memory.conf |
