3fde5196a2f98aeee433167a1c8c2cd6e29b0184
Marek Mihok <marek.mihok@radixal.net>
2022-06-14 00:48:46 +0200

    pridany port monitor

--- create_nic_conf.sh
+++ create_nic_conf.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#echo $pocitadlo $pozicia $nic
-#echo $1 $2 $3
+#echo $pocitadlo $pozicia_y $nic $pozicia_x $min_sirka
+#echo $1 $2 $3 $4 $5
 filename="nic-"$1".conf"
 substring=w
-posun=$2
+pozicia_y=$2
+pozicia_x=$4
+min_sirka=$5
 cp nic_default.conf $filename
 
 if [ "${3/$substring}" = "$3" ] ; then
@@ -15,8 +17,15 @@ eth=wifi_default.conf
 fi
 cat "$eth" >> "$filename"
 
-search="posun_y";
-sed -i "s/$search/$posun/" $filename
+search="pozicia_y";
+sed -i "s/$search/$pozicia_y/" $filename
+
+search="pozicia_x";
+sed -i "s/$search/$pozicia_x/" $filename
+
+search="min_sirka";
+sed -i "s/$search/$min_sirka/" $filename
+
 
 search="adapter_nazov";
 sed -i "s/$search/$3/g" $filename
--- nic.sh
+++ nic.sh
@@ -2,22 +2,24 @@
 rm nic-*.conf
 rm autoconfig.sh
 pocitadlo=1
-pozicia=30
+pozicia_y=20
+pozicia_x=1400
+min_sirka=500
 substring=w
 for nic in $(ip a | grep -B1 "ether" | grep -v ether | awk '{print $2}' | cut -d":" -f1)
 do 
 
 if [ "${nic/$substring}" = "$nic" ] ; then
-#  echo "${substring} is not in ${3}"
+#  echo "${substring} is not in ${nic}"
 vyska=137
 else
-#  echo "${substring} was found in ${3}"
+#  echo "${substring} was found in ${nic}"
 vyska=182
 fi
 
-  #echo $pocitadlo $pozicia $nic
-  source create_nic_conf.sh $pocitadlo $pozicia $nic
-  (( pozicia += vyska ))
+  #echo $pocitadlo $pozicia_y $nic
+  source create_nic_conf.sh $pocitadlo $pozicia_y $nic $pozicia_x $min_sirka
+  (( pozicia_y += vyska ))
   (( pocitadlo += 1 ))
 done
 
@@ -25,9 +27,19 @@ filename=global_network_default.conf
 cp nic_default.conf $filename
 cat network_default.conf >> "$filename"
 
+pozicia_y=20
+search="pozicia_y";
+sed -i "s/$search/$pozicia_y/" $filename
 
-search="posun_y";
-sed -i "s/$search/$pozicia/" $filename
+pozicia_x=20
+search="pozicia_x";
+sed -i "s/$search/$pozicia_x/" $filename
+
+min_sirka=800
+search="min_sirka";
+sed -i "s/$search/$min_sirka/" $filename
+
+
 echo "conky -q -c $filename" >> autoconfig.sh
 
 source autoconfig.sh
