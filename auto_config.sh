dc0bd8462263f28843a35c4f3be51f98e04e9cdc
Marek Mihok <marek.mihok@radixal.net>
2022-06-14 18:44:08 +0200

    gitignore

---
+++ auto_config.sh
@@ -0,0 +1,7 @@
+conky -q -c auto_nic-1.conf |
+conky -q -c auto_nic-2.conf |
+conky -q -c auto_nic-3.conf |
+conky -q -c auto_nic-4.conf |
+conky -q -c auto_nic-5.conf |
+conky -q -c auto_port_monitor.conf |
+conky -q -c auto_logy.conf
