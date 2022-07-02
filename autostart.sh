ea68991358497c9a619ce7786548b213c32cd294
Marek Mihok <pihi77@gmail.com>
2022-07-02 14:27:44 +0200

    pridany offset pre os x

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,9 @@
 cd $(dirname $0)
 killall conky
 rm -r ./auto_generated/auto_*
+offset_x=3840
+offset_y=0
+
 res_x=$(cat /sys/class/graphics/*/virtual_size | cut -d"," -f1)
 res_y=$(cat /sys/class/graphics/*/virtual_size | rev | cut -d"," -f1 | rev)
 
@@ -11,24 +14,24 @@ dve_tretiny=$((tretina + tretina))
 #create_conf.sh conf_name text_name offset_x offset_y size_x size_y [flag (last, network)]
 
 # screen
-source ./components/create_conf.sh screen screen 0 0 $res_x $res_y
+source ./components/create_conf.sh screen screen $((offset_x+0)) $((offset_y+0)) $res_x $res_y
 
 #prva tretina
-source ./components/create_conf.sh default network $((dve_tretiny+20)) 10 $sirka_okna 30
-source ./components/scan_nic.sh default $((dve_tretiny+20)) 70 $sirka_okna 0
-source ./components/create_conf.sh default process $((dve_tretiny+20)) 620 $sirka_okna 30
+source ./components/create_conf.sh default network $((offset_x+dve_tretiny+20)) 10 $sirka_okna 30
+source ./components/scan_nic.sh default $((offset_x+dve_tretiny+20)) 70 $sirka_okna 0
+source ./components/create_conf.sh default process $((offset_x+dve_tretiny+20)) 620 $sirka_okna 30
 
 #druha tretina
-source ./components/create_conf.sh default system_info $((tretina+20)) 10 $sirka_okna 100 
-source ./components/create_conf.sh default storage $((tretina+20)) 300 $sirka_okna 300
-source ./components/create_conf.sh default memory $((tretina+20)) 597 $sirka_okna 300 
+source ./components/create_conf.sh default system_info $((offset_x+tretina+20)) 10 $sirka_okna 100 
+source ./components/create_conf.sh default storage $((offset_x+tretina+20)) 300 $sirka_okna 300
+source ./components/create_conf.sh default memory $((offset_x+tretina+20)) 597 $sirka_okna 300 
 
 #tretia tretina
-source ./components/create_conf.sh default port_monitor 10 10 $sirka_okna 300
-source ./components/create_conf.sh default cpu 10 650 $sirka_okna 300
+source ./components/create_conf.sh default port_monitor $((offset_x+10)) 10 $sirka_okna 300
+source ./components/create_conf.sh default cpu $((offset_x+10)) 650 $sirka_okna 300
 
 #spodna polovica
-source ./components/create_conf.sh default logs 20 1020 1880 300 last
+source ./components/create_conf.sh default logs $((offset_x+20)) 1020 1880 300 last
 
 # run conky
 source ./auto_generated/auto_config.sh
